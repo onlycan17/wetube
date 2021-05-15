56c212a45dd1a2b7d4ed71e5238d55750b5b800b
onlycan17 <onlycan17@gmail.com>
2021-05-15 15:45:33 +0900

    20210515 second update

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {localsMiddleware} from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(localsMiddleware);
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+export const join = (req,res) => res.render("Join",{pageTitle:'Join'});
+export const login = (req, res) => res.render("Login",{pageTitle:'Login'});
+export const logout = (req, res) => res.render("Logout",{pageTitle:'Logout'});
+
+export const users = (req, res) => res.render("users",{pageTile:'Users'});
+export const userDetail = (req, res) => res.render("userDetail",{pageTitle:'User Detail'});
+export const editProfile = (req, res) => res.render("editProfile",{pageTitle:'Edit Profile'});
+export const changePassword = (req, res) => res.render("changePassword",{pageTitle:'Change Password'});
