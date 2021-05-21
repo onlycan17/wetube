270042cae9a35ebb776a9341ffde6f5dce16b399
onlycan17 <onlycan17@gmail.com>
2021-05-21 10:20:14 +0900

    20210521 update

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,28 @@
 import express from "express";
 import routes from "../routes";
-import {videos,upload,videoDetail,editVideo,deleteVideo} from "../controllers/videoController"
+import {getUpload,
+        postUpload,
+        videoDetail,
+        deleteVideo,
+        getEditVideo,
+        postEditVideo
+    } from "../controllers/videoController";
+import {upload, uploadVideo} from "../middlewares";
 
 const videoRouter = express.Router();
+//Upload
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
-videoRouter.get(routes.videoDetail, videoDetail);
+//Video Detail
+videoRouter.get(routes.videoDetail(), videoDetail);
+
+//Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
+
 
 export default videoRouter;
